7e2a8a8722a139cbc21423336157ab8b68412262
Oksana <myzove@mail.ru>
2020-08-20 22:04:13 +0300

    Updates

---
+++ precache-manifest.25f0878a9f93ef98b4c9e571e86354b8.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9fdc190e054efef0698ff98451d0289",
+    "url": "/strive-template/index.html"
+  },
+  {
+    "revision": "cf29fa0305abf244ceb6",
+    "url": "/strive-template/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ef8b8015b2ac31b48e27",
+    "url": "/strive-template/static/css/main.c7ce4775.chunk.css"
+  },
+  {
+    "revision": "cf29fa0305abf244ceb6",
+    "url": "/strive-template/static/js/2.bca7f1c9.chunk.js"
+  },
+  {
+    "revision": "1ba9f94300e0363389718dc9d0c317f1",
+    "url": "/strive-template/static/js/2.bca7f1c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8b8015b2ac31b48e27",
+    "url": "/strive-template/static/js/main.760fb47d.chunk.js"
+  },
+  {
+    "revision": "5e6746cbf92bb04cd749",
+    "url": "/strive-template/static/js/runtime-main.45c7149d.js"
+  },
+  {
+    "revision": "7808c0d27cabd5ec6de1124b1af00df7",
+    "url": "/strive-template/static/media/Micheal.7808c0d2.png"
+  },
+  {
+    "revision": "439b70ba57d7b63ba34fedcbde1308c1",
+    "url": "/strive-template/static/media/Valerio.439b70ba.jpeg"
+  },
+  {
+    "revision": "b7ec461cd0bd2dab5491c593bf6323df",
+    "url": "/strive-template/static/media/back-drop.b7ec461c.svg"
+  },
+  {
+    "revision": "e9fcb24d2baf2342285f92b6b940762e",
+    "url": "/strive-template/static/media/boot.e9fcb24d.png"
+  },
+  {
+    "revision": "6810d481c2449b4bed0233daa640b1e3",
+    "url": "/strive-template/static/media/combinator.6810d481.png"
+  },
+  {
+    "revision": "81c2a96c92ee97c0fc2e0f4c6b7e41c5",
+    "url": "/strive-template/static/media/dot.81c2a96c.svg"
+  },
+  {
+    "revision": "19b4df54444817a7b7e0b1407c461f59",
+    "url": "/strive-template/static/media/drops.19b4df54.svg"
+  },
+  {
+    "revision": "13383e02ce7a3c3168bfcd199e1a9f4c",
+    "url": "/strive-template/static/media/faina.13383e02.png"
+  },
+  {
+    "revision": "5861abf4e97e814076c629464632857b",
+    "url": "/strive-template/static/media/git.5861abf4.png"
+  },
+  {
+    "revision": "972ab11b799f07ce01d5f1b0a15fcdfa",
+    "url": "/strive-template/static/media/head-1.972ab11b.svg"
+  },
+  {
+    "revision": "86144cca4a54987f01ffcdefdeb57b46",
+    "url": "/strive-template/static/media/html.86144cca.png"
+  },
+  {
+    "revision": "359d0cc6a6b20f0f0e0c4e6c08c17929",
+    "url": "/strive-template/static/media/logo-name.359d0cc6.png"
+  },
+  {
+    "revision": "f8b766d0ca6df5a478b9b11311fb198d",
+    "url": "/strive-template/static/media/luca.f8b766d0.png"
+  },
+  {
+    "revision": "270f55fffea36219b66a63fbe03ff497",
+    "url": "/strive-template/static/media/node.270f55ff.png"
+  },
+  {
+    "revision": "38ff18a5105bfeb90981b383730f0221",
+    "url": "/strive-template/static/media/pic.38ff18a5.svg"
+  },
+  {
+    "revision": "c82fdff2a782f3f1a18a2783568629f8",
+    "url": "/strive-template/static/media/react.c82fdff2.png"
+  },
+  {
+    "revision": "4a87f200a08a840d0a8ec010cd2c2b80",
+    "url": "/strive-template/static/media/sql.4a87f200.png"
+  },
+  {
+    "revision": "d698556070ee6ade5a65e96ec25b7e74",
+    "url": "/strive-template/static/media/techcrunch.d6985560.png"
+  },
+  {
+    "revision": "e21b506b9bd74d671f6c78253ae60aea",
+    "url": "/strive-template/static/media/typescript.e21b506b.png"
+  },
+  {
+    "revision": "207af25a5b3ff94899fe69591148ca82",
+    "url": "/strive-template/static/media/vec-1.207af25a.png"
+  },
+  {
+    "revision": "77a48dce40addb40e8b70d0d5defa304",
+    "url": "/strive-template/static/media/vec-2.77a48dce.png"
+  },
+  {
+    "revision": "a58b49b44bef718643710761ea0a3625",
+    "url": "/strive-template/static/media/web.a58b49b4.png"
+  }
+]);
